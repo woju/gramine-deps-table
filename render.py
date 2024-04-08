woju/gramine-deps-table

commit bfdec8c6c4b7cf4598e520a2edde5ada61a2ba0d

render.py: Retry after an exponential delay
Signed-off-by: Wojtek Porczyk <woju@invisiblethingslab.com>

render.py
@@ -169,9 +169,17 @@ def main(file):
         for package in packages:
             print(f'package={package}', file=sys.stderr)
             for pkgid in reversed(list(package.all_ids)):
-                resp = http.get(f'https://repology.org/api/v1/projects/{pkgid}/')
-                resp.raise_for_status()
-                pkglist = resp.json()[pkgid]
+                delay = 1
+                while True:
+                    try:
+                        resp = http.get(f'https://repology.org/api/v1/projects/{pkgid}/')
+                        resp.raise_for_status()
+                        pkglist = resp.json()[pkgid]
+                        break
+                    except httpx.ReadTimeout:
+                        print(f'  read timeout, delaying {delay} s', file=sys.stderr)
+                        time.sleep(delay)
+                        delay *= 1.5
 
                 for line in lines.values():
                     for p, pv in line.items():

commit b270fa8438a9dff02ae507e9b4d2ea7759c52664

render.py: Add missing "import time"
Fixes: bfdec8c6c4b7 ("render.py: Retry after an exponential delay")
Signed-off-by: Wojtek Porczyk <woju@invisiblethingslab.com>

render.py
@@ -10,6 +10,7 @@ import datetime
 import pathlib
 import pdb
 import sys
+import time
 
 import click
 import dateutil.parser